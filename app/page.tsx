e3efe52a9eeff49bff3ff9d76ed091e029d0dba0
miguel angel <miguelangeljimenez1047@gmail.com>
2025-06-13 07:28:32 -0500

    FEAT: minor changes ⚒️

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,7 @@
-import { View } from '@/components/view'
-import { ModeSwitch } from '@/components/mode-switch'
-
 export default function Home() {
   return (
     <main className='space-y-4'>
-      <header className='flex items-center gap-4'>
-        <h2>Personajes</h2>
-        <ModeSwitch />
-      </header>
-
-      <View />
+      <h3>Index</h3>
     </main>
   )
 }
