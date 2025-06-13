c0b336984c20c1d4b31e67d2f1d3a577c930eb16
miguel angel <miguelangeljimenez1047@gmail.com>
2025-06-13 07:19:28 -0500

    FEAT: auth routes file ⚒️

---
+++ routes.ts
@@ -0,0 +1,5 @@
+export const authRoutes = ['/login']
+export const protectedRoutes = ['/dashboard', '/edit', '/new']
+
+export const defaultRedirect = '/login'
+export const redirectAfterAuth = '/dashboard'
