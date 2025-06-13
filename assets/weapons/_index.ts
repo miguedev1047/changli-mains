7c40834c9ef7ffa70fa51f6832f4878851ba9870
miguel angel <miguelangeljimenez1047@gmail.com>
2025-06-13 07:21:40 -0500

    FEAT: add weapons icons 🖼️

---
+++ assets/weapons/_index.ts
@@ -0,0 +1,7 @@
+import BoardbladeIcon from '@/assets/weapons/boardblade-icon.png'
+import GuantletsIcon from '@/assets/weapons/guantlets-icon.png'
+import PistolsIcon from '@/assets/weapons/pistols-icon.png'
+import RectifierIcon from '@/assets/weapons/rectifier-icon.png'
+import SwordIcon from '@/assets/weapons/sword-icon.png'
+
+export { BoardbladeIcon, GuantletsIcon, PistolsIcon, RectifierIcon, SwordIcon }
