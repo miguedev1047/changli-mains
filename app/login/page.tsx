9166c1f996b9061a700125ed0eceb543dbe9dec9
miguel angel <miguelangeljimenez1047@gmail.com>
2025-06-13 07:27:34 -0500

    FEAT: add login page 🎨

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from '@/app/login/_components/login-form'
+
+export default function LoginPage() {
+  return (
+    <div className='w-full max-w-sm'>
+      <LoginForm />
+    </div>
+  )
+}
