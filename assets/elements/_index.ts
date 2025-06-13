a2a5a989d4c1479bddb3b6675dadecc8f32d11e8
miguel angel <miguelangeljimenez1047@gmail.com>
2025-06-13 07:22:06 -0500

    FEAT: add elements icons 🖼️

---
+++ assets/elements/_index.ts
@@ -0,0 +1,8 @@
+import AeroIcon from '@/assets/elements/aero-icon.png'
+import ElectroIcon from '@/assets/elements/electro-icon.png'
+import FusionIcon from '@/assets/elements/fusion-icon.png'
+import GlacioIcon from '@/assets/elements/glacio-icon.png'
+import HavocIcon from '@/assets/elements/havoc-icon.png'
+import SpectroIcon from '@/assets/elements/spectro-icon.png'
+
+export { AeroIcon, ElectroIcon, FusionIcon, GlacioIcon, HavocIcon, SpectroIcon }
