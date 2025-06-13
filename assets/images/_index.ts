77a601546a3aae77224c475fe859ba17a13c134b
miguel angel <miguelangeljimenez1047@gmail.com>
2025-06-13 07:21:52 -0500

    FEAT: add general images 🖼️

---
+++ assets/images/_index.ts
@@ -0,0 +1,6 @@
+import CharactersImg from '@/assets/images/characters.png'
+import WeaponsImg from '@/assets/images/weapons.png'
+import EchoesImg from '@/assets/images/echoes.png'
+import MaterialsImg from '@/assets/images/materials.png'
+
+export { CharactersImg, WeaponsImg, EchoesImg, MaterialsImg }
