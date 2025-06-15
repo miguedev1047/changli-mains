5256cf43202d7abf861bbbe2c9467fb87eb98d0b
miguel angel <miguelangeljimenez1047@gmail.com>
2025-06-14 19:20:08 -0500

    FEAT: add card form wrapper component 🎨

---
+++ components/card-form-wrapper/card-form-wrapper.tsx
@@ -0,0 +1,69 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
+import { useRouter } from 'next/navigation'
+import { ChevronLeft } from 'lucide-react'
+
+type CardFormWrapperProps = {
+  title: string
+  description?: string
+  isEditing?: boolean
+  isPending?: boolean
+  canSubmit?: boolean
+  backButton?: boolean
+  disabled?: boolean
+  formId: string
+  children: React.ReactNode
+}
+
+export function CardFormWrapper(props: CardFormWrapperProps) {
+  const {
+    title,
+    description = '',
+    formId,
+    isEditing = false,
+    disabled = false,
+    children,
+    backButton = false,
+  } = props
+  const { back } = useRouter()
+
+  return (
+    <Card>
+      <CardHeader>
+        <div className='flex items-center gap-1'>
+          {backButton && (
+            <Button
+              variant='link'
+              onClick={back}
+              size='icon'
+            >
+              <ChevronLeft />
+            </Button>
+          )}
+          
+          <CardTitle className='text-xl uppercase'>{title}</CardTitle>
+        </div>
+        {description && <CardDescription>{description}</CardDescription>}
+      </CardHeader>
+      
+      <CardContent>{children}</CardContent>
+
+      <CardFooter className='justify-end'>
+        <Button
+          type='submit'
+          form={formId}
+          disabled={disabled}
+        >
+          {isEditing ? 'Guardar' : 'Crear'}
+        </Button>
+      </CardFooter>
+    </Card>
+  )
+}
