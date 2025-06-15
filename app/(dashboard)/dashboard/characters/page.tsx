ae3e9ac6e0e6011575edae1ce04ffd780452ca0b
miguel angel <miguelangeljimenez1047@gmail.com>
2025-06-14 19:21:47 -0500

    FEAT: add dashboard character page 🎨

--- app/(dashboard)/dashboard/characters/page.tsx
+++ app/(dashboard)/dashboard/characters/page.tsx
@@ -1,7 +1,42 @@
+import Link from 'next/link'
+
+import { Plus } from 'lucide-react'
+import { QueryInput } from '@/components/query-input'
+import { Button } from '@/components/ui/button'
+import { CharacterList } from '@/app/(dashboard)/dashboard/characters/_components/character-list'
+import { CharacterFilter } from '@/app/(dashboard)/dashboard/characters/_components/character-filter'
+
 export default function DashboardCharactersPage() {
   return (
-    <div>
-      <h2>Personajes</h2>
-    </div>
+    <>
+      <header className='flex items-center justify-between'>
+        <h2 className='font-black text-xl md:text-2xl uppercase'>
+          Lista de Personajes
+        </h2>
+
+        <div className='flex items-center gap-2'>
+          <QueryInput
+            queryParam='name'
+            placeholder='Buscar personaje'
+          />
+
+          <Button
+            variant='outline'
+            size='sm'
+            asChild
+          >
+            <Link href='/new/character'>
+              <Plus />
+              <span className='hidden lg:inline'>Agregar personaje</span>
+            </Link>
+          </Button>
+        </div>
+      </header>
+
+      <section className='space-y-8'>
+        <CharacterFilter />
+        <CharacterList />
+      </section>
+    </>
   )
 }
