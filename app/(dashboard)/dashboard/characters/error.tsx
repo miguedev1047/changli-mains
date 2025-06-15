9882302553c70ec1e6ccb91bfa68eaae9403bbe4
miguel angel <miguelangeljimenez1047@gmail.com>
2025-06-14 22:09:53 -0500

    FEAT: setup error boundary component & types ⚙️

---
+++ app/(dashboard)/dashboard/characters/error.tsx
@@ -0,0 +1,8 @@
+'use client'
+
+import ErrorBoundaryMessage from '@/components/error-boundary-message/error-boundary-message'
+import { ErrorBoundaryProps } from '@/types/error-boundary'
+
+export default function ErrorPage(props: ErrorBoundaryProps) {
+  return <ErrorBoundaryMessage {...props} />
+}
---
+++ components/error-boundary-message/error-boundary-message.tsx
@@ -0,0 +1,17 @@
+import { ErrorBoundaryProps } from '@/types/error-boundary'
+import { Button } from '@/components/ui/button'
+
+export default function ErrorBoundaryMessage(props: ErrorBoundaryProps) {
+  const { reset } = props
+
+  return (
+    <section className='w-full h-full grid place-content-center'>
+      <div className='flex flex-col items-center gap-4'>
+        <h2 className='text-xl md:text-2xl'>
+          Ha ocurrido un error inesperado.
+        </h2>
+        <Button onClick={() => reset()}>Recargar</Button>
+      </div>
+    </section>
+  )
+}
