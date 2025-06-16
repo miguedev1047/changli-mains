40fa36dc8d71dae2a680bea8105a3007595b97db
miguel angel <miguelangeljimenez1047@gmail.com>
2025-06-16 06:13:36 -0500

    CHORE: add weapon zod schema ⚒️

--- schemas/index.ts
+++ schemas/index.ts
@@ -91,3 +91,47 @@ export const characterSchema = z.object({
     .nonempty('Debes seleccionar al menos un estilo de combate'),
 })
 export type CharacterSchema = z.infer<typeof characterSchema>
+
+export const weaponSchema = z.object({
+  id: z.string().optional(),
+
+  name: z
+    .string({
+      required_error: 'El nombre del arma es obligatorio',
+      invalid_type_error: 'El nombre del arma debe ser una cadena de texto',
+    })
+    .min(2, 'El nombre debe tener al menos 2 caracteres'),
+
+  description: z
+    .string({
+      required_error: 'La descripción del arma es obligatoria',
+      invalid_type_error: 'La descripción debe ser una cadena de texto',
+    })
+    .min(10, 'La descripción debe tener al menos 10 caracteres'),
+
+  icon_image: z
+    .string({
+      required_error: 'La URL de la imagen es obligatoria',
+      invalid_type_error: 'La URL de la imagen debe ser una cadena de texto',
+    })
+    .url('El formato de la URL de la imagen no es válido'),
+
+  rarity: z
+    .string({
+      required_error: 'La rareza del arma es obligatoria',
+      invalid_type_error: 'La rareza debe ser un valor válido',
+    })
+    .min(1, 'Debes seleccionar una rareza'),
+
+  weapon_type: z
+    .string({
+      required_error: 'El tipo de arma es obligatorio',
+      invalid_type_error: 'El tipo de arma seleccionado no es válido',
+    })
+    .min(1, 'Debes seleccionar un tipo de arma'),
+
+  is_new: z.boolean(),
+
+  is_public: z.boolean(),
+})
+export type WeaponSchema = z.infer<typeof weaponSchema>
