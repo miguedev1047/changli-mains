97762e552a5b194f6bb6826e6c1ab37a8603e25a
miguel angel <miguelangeljimenez1047@gmail.com>
2025-06-16 06:14:43 -0500

    CHORE: remove character card component ⚒️

--- components/character-card/character-card.props.ts
+++
@@ -1,8 +0,0 @@
-import { type trpc } from "@/app/_trpc/client"
-import { type inferOutput } from "@trpc/tanstack-react-query"
-
-export type CharactersWithRoles = {
-  data: inferOutput<typeof trpc.characters.getUnique>
-  showDeleteButton?: boolean
-  showElementAndWeaponIcons?: boolean
-}
