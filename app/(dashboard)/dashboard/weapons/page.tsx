cedc7f1dedb50518f78666c0e53874bc8639b9b0
miguel angel <miguelangeljimenez1047@gmail.com>
2025-06-16 06:17:34 -0500

    FEAT: add weapon page 🎨

--- app/(dashboard)/dashboard/weapons/page.tsx
+++ app/(dashboard)/dashboard/weapons/page.tsx
@@ -1,7 +1,47 @@
+import Link from 'next/link'
+
+import { QueryInput } from '@/components/query-input'
+import { Button } from '@/components/ui/button'
+import { Plus } from 'lucide-react'
+import { WeaponFilter } from '@/app/(dashboard)/dashboard/weapons/_components/weapon-filter'
+import { Suspense } from 'react'
+import { WeaponList } from '@/app/(dashboard)/dashboard/weapons/_components/weapon-list'
+import { SpinContentLoader } from '@/components/spin-loaders'
+
 export default function DashboardWeaponsPage() {
   return (
-    <div>
-      <h2>Armas</h2>
-    </div>
+    <>
+      <header className='flex items-center justify-between'>
+        <h2 className='font-black text-xl md:text-2xl uppercase'>
+          Lista de armas
+        </h2>
+
+        <div className='flex items-center gap-2'>
+          <QueryInput
+            queryParam='name'
+            placeholder='Buscar arma'
+          />
+
+          <Button
+            variant='outline'
+            size='sm'
+            asChild
+          >
+            <Link href='/new/weapon'>
+              <Plus />
+              <span className='hidden lg:inline'>Agregar arma</span>
+            </Link>
+          </Button>
+        </div>
+      </header>
+
+      <section className='space-y-8'>
+        <WeaponFilter />
+
+        <Suspense fallback={<SpinContentLoader />}>
+          <WeaponList />
+        </Suspense>
+      </section>
+    </>
   )
 }
