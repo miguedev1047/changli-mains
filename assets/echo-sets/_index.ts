7c31bc83e864fece31c02b7e42dec799a58e7a19
miguel angel <miguelangeljimenez1047@gmail.com>
2025-06-17 12:34:03 -0500

    FEAT: add echo sets icons 🖼️

---
+++ assets/echo-sets/_index.ts
@@ -0,0 +1,37 @@
+import CelestialLightImg from '@/assets/echo-sets/celestial-light.png'
+import EmpyreanAnthemImg from '@/assets/echo-sets/empyrean-anthem.png'
+import EternalRadianceImg from '@/assets/echo-sets/eternal-radiance.png'
+import FlamingClawprintImg from '@/assets/echo-sets/flaming-clawprint.png'
+import FreezingFrostImg from '@/assets/echo-sets/freezing-frost.png'
+import FrostyResolveImg from '@/assets/echo-sets/frosty-resolve.png'
+import GustsOfWelkinImg from '@/assets/echo-sets/gusts-of-welkin.png'
+import HavocEclipseImg from '@/assets/echo-sets/havoc-eclipse.png'
+import LingeringTunesImg from '@/assets/echo-sets/lingering-tunes.png'
+import MidnightVeilImg from '@/assets/echo-sets/midnight-veil.png'
+import MoltenRiftImg from '@/assets/echo-sets/molten-rift.png'
+import MoonlitCloudsImg from '@/assets/echo-sets/moonlit-clouds.png'
+import RejuvenatingGlowImg from '@/assets/echo-sets/rejuvenating-glow.png'
+import SierraGaleImg from '@/assets/echo-sets/sierra-gale.png'
+import TidebreakingCourageImg from '@/assets/echo-sets/tidebreaking-courage.png'
+import VoidThunderImg from '@/assets/echo-sets/void-thunder.png'
+import WindwardPilgrimageImg from '@/assets/echo-sets/windward-pilgrimage.png'
+
+export {
+  CelestialLightImg,
+  EmpyreanAnthemImg, 
+  EternalRadianceImg,
+  FlamingClawprintImg,
+  FreezingFrostImg,
+  FrostyResolveImg,
+  GustsOfWelkinImg,
+  HavocEclipseImg,
+  LingeringTunesImg,
+  MidnightVeilImg,
+  MoltenRiftImg,
+  MoonlitCloudsImg,
+  RejuvenatingGlowImg,
+  SierraGaleImg,
+  TidebreakingCourageImg,
+  VoidThunderImg,
+  WindwardPilgrimageImg
+}
