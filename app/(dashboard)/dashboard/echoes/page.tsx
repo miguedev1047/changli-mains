cdecdd1d8d375e3e67a4f8046a3e824af47faf9a
miguel angel <miguelangeljimenez1047@gmail.com>
2025-06-17 12:37:33 -0500

    FEAT: add echo page 🎨

--- app/(dashboard)/dashboard/echoes/page.tsx
+++ app/(dashboard)/dashboard/echoes/page.tsx
@@ -1,7 +1,47 @@
+import Link from 'next/link'
+
+import { QueryInput } from '@/components/query-input'
+import { Button } from '@/components/ui/button'
+import { Plus } from 'lucide-react'
+import { SpinContentLoader } from '@/components/spin-loaders'
+import { Suspense } from 'react'
+import { EchoList } from '@/app/(dashboard)/dashboard/echoes/_components/echo-list'
+import { EchoFilter } from '@/app/(dashboard)/dashboard/echoes/_components/echo-filter'
+
 export default function DashboardEchoesPage() {
   return (
-    <div>
-      <h2>Ecos</h2>
-    </div>
+    <>
+      <header className='flex items-center justify-between'>
+        <h2 className='font-black text-xl md:text-2xl uppercase'>
+          Lista de ecos
+        </h2>
+
+        <div className='flex items-center gap-2'>
+          <QueryInput
+            queryParam='name'
+            placeholder='Buscar eco'
+          />
+
+          <Button
+            variant='outline'
+            size='sm'
+            asChild
+          >
+            <Link href='/new/echo'>
+              <Plus />
+              <span className='hidden lg:inline'>Agregar eco</span>
+            </Link>
+          </Button>
+        </div>
+      </header>
+
+      <section className='space-y-8'>
+        <EchoFilter />
+
+        <Suspense fallback={<SpinContentLoader />}>
+          <EchoList />
+        </Suspense>
+      </section>
+    </>
   )
 }
