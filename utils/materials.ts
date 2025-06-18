d83efdd839d9dc5f1148e67bdf8f59e4eba44897
miguel angel <miguelangeljimenez1047@gmail.com>
2025-06-17 19:04:14 -0500

    FEAT: add material utils func ⚒️

---
+++ utils/materials.ts
@@ -0,0 +1,21 @@
+import { trpc } from '@/app/_trpc/client'
+import { lowerCaseFunc } from '@/helpers/to-lower-str'
+import { inferOutput } from '@trpc/tanstack-react-query'
+
+type MaterialsProps = inferOutput<typeof trpc.materials.getAll>
+type Filters = { [k: string]: string }
+
+export function filterMaterials(material: MaterialsProps, filters: Filters) {
+  if (!material) return []
+
+  const { name, stars } = filters
+
+  return material.filter((item) => {
+    const matcher = [
+      name ? lowerCaseFunc(item.name).includes(lowerCaseFunc(name)) : true,
+      stars ? lowerCaseFunc(item.rarity) === stars : true,
+    ]
+
+    return matcher.every(Boolean)
+  })
+}
