9b1d7a315d45ba00eb8f2f42e6c108e983be0e09
miguel angel <miguelangeljimenez1047@gmail.com>
2025-06-17 19:07:06 -0500

    FEAT: add materials page 🎨

--- app/(dashboard)/dashboard/materials/page.tsx
+++ app/(dashboard)/dashboard/materials/page.tsx
@@ -1,7 +1,47 @@
+import Link from 'next/link'
+
+import { QueryInput } from '@/components/query-input'
+import { SpinContentLoader } from '@/components/spin-loaders'
+import { Button } from '@/components/ui/button'
+import { Plus } from 'lucide-react'
+import { Suspense } from 'react'
+import { MaterialList } from '@/app/(dashboard)/dashboard/materials/_components/material-list'
+import { MaterialFilter } from '@/app/(dashboard)/dashboard/materials/_components/material-filter'
+
 export default function DashboardMaterialsPage() {
   return (
-    <div>
-      <h2>Materiales</h2>
-    </div>
+    <>
+      <header className='flex items-center justify-between'>
+        <h2 className='font-black text-xl md:text-2xl uppercase'>
+          Lista de materiales
+        </h2>
+
+        <div className='flex items-center gap-2'>
+          <QueryInput
+            queryParam='name'
+            placeholder='Buscar material'
+          />
+
+          <Button
+            variant='outline'
+            size='sm'
+            asChild
+          >
+            <Link href='/new/material'>
+              <Plus />
+              <span className='hidden lg:inline'>Agregar material</span>
+            </Link>
+          </Button>
+        </div>
+      </header>
+
+      <section className='space-y-8'>
+        <MaterialFilter />
+
+        <Suspense fallback={<SpinContentLoader />}>
+          <MaterialList />
+        </Suspense>
+      </section>
+    </>
   )
 }
