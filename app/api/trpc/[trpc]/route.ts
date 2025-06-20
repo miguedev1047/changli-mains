8192700bbdd99845075a3b4729611bed93ea04b1
miguel angel <miguelangeljimenez1047@gmail.com>
2025-06-19 21:04:13 -0500

    REFACTOR: update trpc endpoint ⚒️

--- app/api/trpc/[trpc]/route.ts
+++ app/api/trpc/[trpc]/route.ts
@@ -1,13 +1,27 @@
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
-import { appRouter } from '@/server/routers/_index'
-import { createContext } from '@/server/lib/context'
+import { appRouter } from '@/server/api/root'
+import { type NextRequest } from 'next/server'
+import { createTRPCContext } from '@/server/api/trpc'
 
-const handler = (req: Request) =>
+const createContext = async (req: NextRequest) => {
+  return createTRPCContext({ headers: req.headers })
+}
+
+const handler = (req: NextRequest) =>
   fetchRequestHandler({
     endpoint: '/api/trpc',
     req,
     router: appRouter,
-    createContext,
+    createContext: () => createContext(req),
+    onError:
+      process.env.NODE_ENV === 'development'
+        ? ({ path, error }) => {
+            const errorMessage = `❌ tRPC failed on ${path ?? '<no-path>'}: ${
+              error.message
+            }`
+            console.error(errorMessage)
+          }
+        : undefined,
   })
 
 export { handler as GET, handler as POST }
